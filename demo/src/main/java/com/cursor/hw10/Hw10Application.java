06b8e24d19274f791262c51b58ab8ce5cf35226a
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-11-16 22:09:06 +0200

    empty commit

---
+++ demo/src/main/java/com/cursor/hw10/Hw10Application.java
@@ -0,0 +1,13 @@
+package com.cursor.hw10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw10Application.class, args);
+	}
+
+}
